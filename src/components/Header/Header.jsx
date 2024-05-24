09609fd071a3492c3b7bdf841dea766cc1fe13eb
Ajay <91714785+ajaykumar2004@users.noreply.github.com>
2024-05-24 13:11:25 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,6 @@ const Header = () => {
             <Navbar/>
             <div className="header-content flex flex-c text-center text-white">
                 <h2 className="header-title text capitalize">Find your book of choice</h2> <br />
-                <p className="header-text fs-10 fw-3">This is a book searching platform and the preview of that book.</p>
                 <SearchForm/>
             </div>
         </header>
